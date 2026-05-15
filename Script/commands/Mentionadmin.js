41d6c3ecb5d5369d354643e9806eb38957e2ecdb
ariyan782915 <mdsahidin383@gmail.com>
2026-05-15 11:44:39 +0600

    Update Mentionadmin.js

--- Script/commands/Mentionadmin.js
+++ Script/commands/Mentionadmin.js
@@ -10,7 +10,7 @@ module.exports.config = {
 };
 
 module.exports.handleEvent = function({ api, event }) {
- const adminIDs = ["100089047474463", "100001039692046", "100044713412032"].map(String);
+ const adminIDs = ["100089047474463", "100001039692046", "61587302116701"].map(String);
  
  if (adminIDs.includes(String(event.senderID))) return;
 
