69c95c81bb75e40b327b8a72262b2362858d9ca0
ariyan782915 <mdsahidin383@gmail.com>
2026-05-17 15:10:49 +0600

    Update slap.js

--- Script/commands/slap.js
+++ Script/commands/slap.js
@@ -2,43 +2,59 @@ module.exports.config = {
   name: "slap",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
-  description: "Slap the friend tag",
+  credits: "Ariyan",
+  description: "কাউকে থাপ্পড় মারার অ্যানিমেশন মিম",
   commandCategory: "general",
   usages: "slap [Tag someone you want to slap]",
   cooldowns: 5,
+  dependencies: {
+    "axios": "",
+    "fs-extra": ""
+  }
 };
 
-
 module.exports.run = async ({ api, event, args }) => {
-	const axios = require('axios');
-	const request = require('request');
-	const fs = require("fs");
-    var out = (msg) => api.sendMessage(msg, event.threadID, event.messageID);
-  if (!args.join("")) return out("Please tag someone");
-  else
-  return axios.get('https://api.waifu.pics/sfw/slap').then(res => {
-        let getURL = res.data.url;
-        let ext = getURL.substring(getURL.lastIndexOf(".") + 1);
-        var mention = Object.keys(event.mentions)[0];
-                  let tag = event.mentions[mention].replace("@", "");    
-        
- let callback = function () {
-            api.setMessageReaction("👊", event.messageID, (err) => {}, true);
-        api.sendMessage({
-						        body: "Slapped! " + tag + "\n\nবেশি ছাবলামি করলে থাপ্পড় মেরে গাল লাল করে দিব 😾",
-                                          mentions: [{
-          tag: tag,
-          id: Object.keys(event.mentions)[0]
-        }],
-						attachment: fs.createReadStream(__dirname + `/cache/slap.${ext}`)
-					}, event.threadID, () => fs.unlinkSync(__dirname + `/cache/slap.${ext}`), event.messageID)
-				};
- //   }
-        request(getURL).pipe(fs.createWriteStream(__dirname + `/cache/slap.${ext}`)).on("close", callback);
-			})
-    .catch(err => {
-                     api.sendMessage("Failed to generate gif, be sure that you've tag someone!", event.threadID, event.messageID);
+  const axios = require('axios');
+  const fs = require("fs-extra");
+  
+  var out = (msg) => api.sendMessage(msg, event.threadID, event.messageID);
+  
+  if (!args.join("")) return out("কাকে থাপ্পড় মারতে চান, তাকে মেনশন করুন!");
+  
+  try {
+    // waifu.pics এপিআই থেকে থাপ্পড়ের ডেটা নেওয়া
+    const res = await axios.get('https://api.waifu.pics/sfw/slap');
+    let getURL = res.data.url;
+    let ext = getURL.substring(getURL.lastIndexOf(".") + 1);
+    
+    var mention = Object.keys(event.mentions)[0];
+    let tag = event.mentions[mention].replace("@", "");    
+    
+    const path = __dirname + `/cache/slap.${ext}`;
+    
+    // ইমেজ বা জিআইএফ ডাউনলোডের আধুনিক ও নিরাপদ পদ্ধতি
+    const imageBuffer = await axios.get(getURL, { responseType: 'arraybuffer' });
+    fs.writeFileSync(path, Buffer.from(imageBuffer.data, 'utf-8'));
+    
+    // রিয়্যাকশন দেওয়া
+    api.setMessageReaction("👊", event.messageID, (err) => {}, true);
+    
+    // মেসেজ পাঠানো
+    return api.sendMessage({
+      body: `ঐ ${tag}, আরিয়ান তোর গালে কষে একটা থাপ্পড় মারলো! 👋💥\n\nবেশি ছাবলামি করলে থাপ্পড় মেরে গাল লাল করে দিব 😾`,
+      mentions: [{
+        tag: tag,
+        id: mention
+      }],
+      attachment: fs.createReadStream(path)
+    }, event.threadID, () => {
+      // ক্যাশ মেমোরি ক্লিয়ার রাখা যেন সার্ভার ডাউন না হয়
+      if (fs.existsSync(path)) fs.unlinkSync(path);
+    }, event.messageID);
+
+  } catch (err) {
+    console.log(err);
+    api.sendMessage("API বা সার্ভার থেকে ইমেজ জেনারেট করতে সমস্যা হচ্ছে! আবার চেষ্টা করুন।", event.threadID, event.messageID);
     api.setMessageReaction("☹️", event.messageID, (err) => {}, true);
-                  })     
+  }     
 }
